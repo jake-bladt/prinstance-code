087199bf8a3e7e8186a96efbe1eff5165eee52dc
Jake <jake@jakebladt.com>
2020-03-10 22:04:34 +0000

    now ignore state

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
 	region = "us-east-1"
-	ami    = "ami-07ebfd5b3428b6f4d"
-	instance_type = "t2.micro"
+	
+	resource "aws_instance" "pri_host" {	
+		ami    = "ami-07ebfd5b3428b6f4d"
+		instance_type = "t2.micro"
+	}
 }
 
