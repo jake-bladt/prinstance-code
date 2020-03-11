99ea5be8449109f7857ae8a1140f0824859637e2
Jake <jake@jakebladt.com>
2020-03-11 01:23:50 +0000

    fix embedding

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
 	region = "us-east-1"
-	
-	resource "aws_instance" "pri_host" {	
-		ami    = "ami-07ebfd5b3428b6f4d"
-		instance_type = "t2.micro"
-	}
+}	
+
+resource "aws_instance" "prinstance_host" {
+	instance_type = "t2.micro"
+	ami           = "ami-07ebfd5b3428b6f4d"
 }
 
