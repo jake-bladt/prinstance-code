7bd324bd80af0fcf68176742c456a7c335dc0fb3
Jake <jake@jakebladt.com>
2020-03-11 17:45:06 +0000

    start building autoscaling group

--- main.tf
+++ main.tf
@@ -8,3 +8,19 @@ resource "aws_instance" "prinstance_host" {
 	key_name      = "tops"
 }
 
+resource "aws_placement_group" "prinstance_pg" {
+	name       = "prinstance"
+	strategy   = "cluster"
+}
+
+resource "aws_autoscaling_group" "prinstance_asg" {
+	name                      = "prinstance-asg"
+	max_size                  =  1
+	min_size                  =  1
+	health_check_grace_period = 300
+	health_check_type         = "ELB"
+	desired_capacity          = 1
+	force_delete              = true
+	placement_group           = "${aws_placement_group.prinstance_pg.id}"
+}
+
